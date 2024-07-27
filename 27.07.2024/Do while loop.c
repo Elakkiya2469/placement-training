f5deac96cf884d832afe924cb8a7cde57e40802c
Elakkiya2469 <151144427+Elakkiya2469@users.noreply.github.com>
2024-07-27 17:25:59 +0530

    Create Do while loop.c

---
+++ 27.07.2024/Do while loop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    printf("\n\n\t\tStudytonight - Best place to learn\n\n\n");
+    int i = 10;    
+    do 
+    {
+        printf("i = %d\n",i);
+        i = i-1;    
+    }
+    while(i > 0);
+    printf("\n\The value of i after exiting the loop is %d\n\n", i);
+    printf("\n\n\t\t\tCoding is Fun !\n\n\n");
+    return 0;
+}
