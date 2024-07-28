688b165a923413f3b9fe03c3800ee72aae845e5a
Elakkiya2469 <151144427+Elakkiya2469@users.noreply.github.com>
2024-07-28 17:59:27 +0530

    Create Vowels.py

---
+++ 28.07.2024/Vowels.py
@@ -0,0 +1,9 @@
+def count_vowels(s):
+    vowels = "aeiouAEIOU"
+    count = 0
+    for char in s:
+        if char in vowels:
+            count += 1
+    return count
+string = input("Enter a string: ")  
+print(f"Number of vowels: {count_vowels(string)}")  
