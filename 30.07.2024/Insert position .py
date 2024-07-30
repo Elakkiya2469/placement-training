28576a14cb161d23151a7f46f035bc1accfc220f
Elakkiya2469 <151144427+Elakkiya2469@users.noreply.github.com>
2024-07-30 21:14:11 +0530

    Create Insert position .py

---
+++ 30.07.2024/Insert position .py
@@ -0,0 +1,11 @@
+def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) 
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left
