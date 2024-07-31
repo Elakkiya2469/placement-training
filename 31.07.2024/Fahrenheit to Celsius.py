e920de6f3fb5a32d8e49067b427addc8030a3afc
Elakkiya2469 <151144427+Elakkiya2469@users.noreply.github.com>
2024-07-31 22:33:52 +0530

    Create Fahrenheit to Celsius.py

---
+++ 31.07.2024/Fahrenheit to Celsius.py
@@ -0,0 +1,6 @@
+def celsius_to_fahrenheit(celsius):
+    return celsius * 9/5 + 32
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
+print(celsius_to_fahrenheit(25))
+print(fahrenheit_to_celsius(77))
