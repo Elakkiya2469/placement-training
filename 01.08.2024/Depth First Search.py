7c2d15793fd46e684d06edf3a2ae9a700a47fe83
Elakkiya2469 <151144427+Elakkiya2469@users.noreply.github.com>
2024-08-01 21:34:26 +0530

    Create Depth First Search.py

---
+++ 01.08.2024/Depth First Search.py
@@ -0,0 +1,17 @@
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    print(start)
+    for next_node in graph[start] - visited:
+        dfs(graph, next_node, visited)
+    return visited
+graph = {
+    'A': {'B', 'C'},
+    'B': {'A', 'D', 'E'},
+    'C': {'A', 'F'},
+    'D': {'B'},
+    'E': {'B', 'F'},
+    'F': {'C', 'E'}
+}
+dfs(graph, 'A')
