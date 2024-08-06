d606d9bbfadd049d13fa87813c20810042230ff8
Elakkiya2469 <151144427+Elakkiya2469@users.noreply.github.com>
2024-08-06 22:01:36 +0530

    Create Append.py

---
+++ 06.08.2024/Append.py
@@ -0,0 +1,8 @@
+f=open("random_number.txt","a")
+n=input("Enter the data to append (separated by comma(,) :\n")
+f.write(n)
+f.close()
+f=open("random_number.txt","r")
+print("the updated random numbers in the file :")
+print(f.read())
+f.close()
